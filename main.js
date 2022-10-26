d2acd9c13cd8359fd4a40130e54e3c412705563a
Danizoo <56428736+Danizoo@users.noreply.github.com>
2022-10-26 20:36:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function genRandomNum(){
     fetch('/Chapters/Chap' + r + 'en1.txt').then(response=>response.text()).then(data=>{
       randomChapEn1.innerText = data;
     })
-    fetch('Chapters/Chap1ch.txt').then(response=>response.text()).then(data=>{
+    fetch('https://raw.githubusercontent.com/Danizoo/RandomTao/main/Chapters/Chap1ch.txt').then(response=>response.text()).then(data=>{
       randomChapCh.innerText = data;
     })
 }
